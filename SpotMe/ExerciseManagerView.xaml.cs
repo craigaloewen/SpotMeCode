b46e97762f716c03bc6c01ade9e06048e8b0b407
Jhony Guan <jhony.guan@gmail.com>
2017-07-23 01:52:28 -0700

    ExerciseManager UI

--- SpotMe/ExerciseManagerView.xaml.cs
+++ SpotMe/ExerciseManagerView.xaml.cs
@@ -26,5 +26,140 @@ namespace SpotMe
 
             ExerciseListBox.ItemsSource = ExerciseManager.GetExerciseNames();
         }
+
+        public void OpenExercise (object sender, RoutedEventArgs e)
+        {
+            string exerciseName = ExerciseListBox.SelectedValue.ToString();
+            
+            if (exerciseName != "")
+            {
+                ClearForms();
+                Exercise openedExercise = ExerciseManager.LoadExercise(exerciseName);
+
+                ExerciseNameTxtBox.Text = openedExercise.name;
+                ClassifierListBox.ItemsSource = openedExercise.GetClassifierNames();
+            }
+        }
+
+        public void DeleteExercise(object sender, RoutedEventArgs e)
+        {
+            string exerciseName = ExerciseListBox.SelectedValue.ToString();
+
+            if (exerciseName != "")
+            {
+                ExerciseManager.DeleteExercise(exerciseName);
+
+                // Reload Exercise List
+                ExerciseListBox.ItemsSource = ExerciseManager.GetExerciseNames();
+                ClearForms();
+            }
+        }
+
+        public void ClearForms(object sender, RoutedEventArgs e)
+        {
+            ClearForms();
+        }
+
+        public void ClearForms()
+        {
+            ClearExerciseForms();
+            ClearClassifierForms();
+        }
+
+        public void ClearExerciseForms()
+        {
+            ExerciseNameTxtBox.Clear();
+            ClassifierListBox.ItemsSource = null;
+        }
+
+        public void ClearClassifierForms()
+        {
+            ClassifierNameTxtBox.Clear();
+            ClassifierMessageTxtBox.Clear();
+        }
+
+        public void SaveExercise(object sender, RoutedEventArgs e)
+        {
+            string exerciseName = ExerciseNameTxtBox.Text;
+
+            if (exerciseName != "")
+            {
+                Exercise newExercise = new Exercise(exerciseName);
+                ExerciseManager.SaveExercise(newExercise);
+
+                // Reload Exercise List
+                ExerciseListBox.ItemsSource = ExerciseManager.GetExerciseNames();
+                ClearForms();
+            }
+        }
+
+        public void OpenClasifier(object sender, RoutedEventArgs e)
+        {
+            string classifierName = ClassifierListBox.SelectedValue.ToString();
+            string exerciseName = ExerciseNameTxtBox.Text;
+
+            if (classifierName != "" && exerciseName != "")
+            {
+                ClearClassifierForms();
+                Exercise openedExercise = ExerciseManager.LoadExercise(exerciseName);
+                Classifier openedClassifier = openedExercise.GetClassifierByName(classifierName);
+
+                ClassifierNameTxtBox.Text = openedClassifier.name;
+
+                if (openedClassifier.form == SkeletonForm.Contracted)
+                {
+                    ClassifierContractedFormBtn.IsChecked = true;
+                }
+                else
+                {
+                    ClassifierExtendedFormBtn.IsChecked = true;
+                }
+
+                ClassifierMessageTxtBox.Text = openedClassifier.message;
+            }
+        }
+
+        public void SaveClassifier(object sender, RoutedEventArgs e)
+        {
+            string classifierName = ClassifierNameTxtBox.Text;
+            string exerciseName = ExerciseNameTxtBox.Text;
+
+            if (classifierName != "" && exerciseName != "")
+            {
+                Exercise openedExercise = ExerciseManager.LoadExercise(exerciseName);
+                SkeletonForm newClassifierForm = SkeletonForm.Contracted;
+
+                if (ClassifierExtendedFormBtn.IsChecked == true)
+                {
+                    newClassifierForm = SkeletonForm.Extended;
+                }
+
+                openedExercise.AddClassifier(newClassifierForm, classifierName, ClassifierMessageTxtBox.Text);
+                ExerciseManager.SaveExercise(openedExercise);
+
+                // Reload Classifier List
+                ClassifierListBox.ItemsSource = openedExercise.GetClassifierNames();
+                ClearClassifierForms();
+            }
+        }
+
+        public void DeleteClassifier(object sender, RoutedEventArgs e)
+        {
+            string classifierName = ClassifierNameTxtBox.Text;
+            string exerciseName = ExerciseNameTxtBox.Text;
+
+            if (classifierName != "" && exerciseName != "")
+            {
+                Exercise openedExercise = ExerciseManager.LoadExercise(exerciseName);
+                Classifier openedClassifier = openedExercise.GetClassifierByName(classifierName);
+
+                openedExercise.DeleteClassifier(openedClassifier.id);
+                ExerciseManager.SaveExercise(openedExercise);
+
+                // Reload Classifier List
+                ClassifierListBox.ItemsSource = openedExercise.GetClassifierNames();
+                ClearClassifierForms();
+            }
+        }
     }
 }
